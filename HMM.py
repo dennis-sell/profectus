9b14a12d89873ef8df2713b75905ddc5557817a8
Dennis Sell <sell.dennis@gmail.com>
2013-06-21 23:43:02 -0700

    Implemented validation of HMMs

--- HMM.py
+++ HMM.py
@@ -18,3 +18,20 @@ class HiddenMarkovModel(object):
     self.emmisions = numpy.matrix(emmissions)
     self.N = emmisions.shape()[0]   # Number of states
     self.M = emmisions.shape()[1]   # Number of obeservations
+
+    def is_valid(self):
+      M = self.M
+      N = self.N
+      epsilon = 0.000001
+      # Creates a list of requirements. Then checks if the list meets all of them.
+      reqs = [initial.shape == (1,N),
+              transitions.shape == (N,N),
+              emmissions.shape == (M,N),
+              abs(initial.sum() - 1) < epsilon]
+
+      transition_row_sums = transitions.sum(1).flatten().tolist()[0]
+      reqs.extend([abs(row_sum - 1) < epsilon for row_sum in transition_row_sums])
+      emmision_row_sums = emmisions.sum(1).flatten().tolist()[0]
+      reqs.extend([abs(row_sum - 1) < epsilon for row_sum in emmision_row_sums])
+      return reqs.count(False) > 0
+
