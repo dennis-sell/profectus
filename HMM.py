bc8eedf4866cfd201f7247e261a49235ce4eb57b
Dennis Sell <sell.dennis@gmail.com>
2013-06-21 22:52:32 -0700

    added comments

--- HMM.py
+++ HMM.py
@@ -2,9 +2,19 @@ import math
 import numpy
 
 class HiddenMarkovModel(object):
+  
+
   def __init__(initial, transitions, emmissions):
+    """ initial - initial[x] = chance that initial state is state x. (1 x N matrix)
+        transitions - transitions[x][y] = chance that state will transition to y, given that the
+          model is currently at state x. (N x N matrix)
+        emmissions - emmisions[s][o] = chance that observation o will occur given that the model
+          is at state s. (M x N matrix)
+        N = number of states
+        M = number of observations
+    """
     self.initial = numpy.matrix(initial)
     self.transitions = numpy.matrix(transitions)
     self.emmisions = numpy.matrix(emmissions)
-    self.N = emmisions.shape()[0]
-    self.M = emmisions.shape()[1]
+    self.N = emmisions.shape()[0]   # Number of states
+    self.M = emmisions.shape()[1]   # Number of obeservations
