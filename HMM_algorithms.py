e398aa6348ebcbf5ad99644448eccee0b7809729
Dennis Sell <sell.dennis@gmail.com>
2013-07-09 00:24:14 -0700

    Implemented Vitterbi Algorithm

--- HMM_algorithms.py
+++ HMM_algorithms.py
@@ -1,4 +1,5 @@
 import numpy
+import operator
 
 import HMM
 
@@ -20,4 +21,25 @@ def probability_of_observations(hmm, observations):
 
 
 def vitterbi(hmm, observations):
-  pass
+  T = len(observations)
+  
+  # Initialization
+  first_observation = observations[0]
+  delta = [hmm.initial[0,state] * hmm.emissions[state, first_observation] for state in hmm.states]
+  omega = [[0] * hmm.N]
+  # Recursion
+  for t in range(1, T):
+    old_delta = delta
+    o = observations[t]
+    delta = []
+    omega_row = []
+    for s2 in hmm.states:
+      max_index, max_value = max(enumerate(old_delta[s1] * hmm.transitions[s1, s2] for s1 in hmm.states), key=operator.itemgetter(1))
+      omega_row.append(max_index)
+      delta.append(max_value * hmm.emissions[s2, o])  
+    omega.append(omega_row)
+  
+  states = [delta.index(max(delta))]
+  for t in range(1, T):
+    states.insert(0, omega[t][states[0]])
+  return state
