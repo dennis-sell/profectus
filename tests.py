a2427c0c94e9a2726fb14bbc283aafeb3fd0de3f
Dennis's MacBook Pro <sell.dennis@gmail.com>
2013-07-08 18:41:41 -0700

    Added test

--- tests.py
+++ tests.py
@@ -1,6 +1,6 @@
 import HMM
 import unittest
-import HMM_algorithms
+import HMM_algorithms as Algs
 
 class TestSequenceFunctions(unittest.TestCase):
 
@@ -11,19 +11,24 @@ class TestSequenceFunctions(unittest.TestCase):
     emissions = [[.0, .3, .7],
                  [.5, .3, .2]]
     self.model1 = HMM.HiddenMarkovModel(initial, transitions, emissions)
-    
+
     emissions = transitions
-    self.model2 = HMM.HiddenMarkovModel(initial, transitions, emissions) 
+    self.model2 = HMM.HiddenMarkovModel(initial, transitions, emissions)
 
   def test_is_valid(self):
     self.assertTrue(self.model1.is_valid())
+
+  def test_is_valid2(self):
     self.assertTrue(self.model2.is_valid())
 
   def test_calculate_probability(self):
     observations = [0,2,1]
-    self.assertAlmostEquals(HMM_algorithms.probability_of_observations(self.model1, observations),  .0375)
+    self.assertAlmostEquals(Algs.probability_of_observations(self.model1, observations),  .0375)
 
-    
+  def test_calculate_probability2(self):
+    observations = [1,1]
+    self.assertAlmostEquals(Algs.probability_of_observations(self.model2, observations), .1175)
+
 
 if __name__ == "__main__":
   unittest.main()
