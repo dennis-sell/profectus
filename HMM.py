20e44d21db5885cc131fddc6f43a43dbf18a841e
Dennis's MacBook Pro <sell.dennis@gmail.com>
2013-07-13 14:49:02 -0700

    Implemented HMM creator from observations with shown states

--- HMM.py
+++ HMM.py
@@ -5,6 +5,8 @@ import numpy
 
 EPSILON = 0.000001
 
+Model = collections.namedtuple('Model', ['hmm', 'o_mapper', 's_mapper'])
+
 class Mapping(dict):
     def __len__(self):
         return dict.__len__(self) / 2
@@ -13,7 +15,6 @@ class Mapping(dict):
         dict.__setitem__(self, key, value)
         dict.__setitem__(self, value, key)
 
-Model = collections.namedtuple('Model', ['model', 'o_mapper', 's_mapper'])
 
 def get_mapping(list_of_sequences):
   all_elements = set(itertools.chain(*list_of_sequences))
@@ -25,34 +26,52 @@ def get_mapping(list_of_sequences):
   mapped_sequences = []
   for sequence in list_of_sequences:
     mapped_sequences.append([mapper[elem] for elem in sequence])
-  return mapper, mapped_sequences
+  return mapper, mapped_sequences, len(all_elements)
 
-def ParameterEstimation(observations, states):
+
+def stochasticize(matrix):
+  new_matrix = []
+  for row in matrix:
+    row_total = float(sum(row))
+    if row_total != 0:
+      stochastic_row = [count/row_total for count in row]
+    else:
+      stochastic_row = row
+    new_matrix.append(stochastic_row)
+  return new_matrix
+
+
+def parameter_estimation(states, observations):
   # Checks if inputs have the same size.
   if len(observations) != len(states) or not observations:
     raise ValueError("Invalid number of states and observation sequences")
   if not all(len(o) == len(s) and o for o,s in zip(observations, states)):
     raise  ValueError("Observations and states do not match in size.")
 
-  total_sequences = len(states)
-  total_instances = len(itertools.chain(*states))
+  state_mapper, mapped_states, num_states = get_mapping(states)
+  observation_mapper, mapped_observations, num_observations = get_mapping(observations)
 
-  state_mapper, mapped_states = get_mapping(states)
-  observation_mapper, mapped_observations = get_mapping(observations)
-
-  first_states = [sequence[0] for sequence in states]
+  # Get initial state matrix
+  first_states = [sequence[0] for sequence in mapped_states]
   initial_counts = collections.Counter(first_states)
-  num_states = len(Mapping)
-  initial = [float(initial_counts[s])/total_sequences for s in range(num_states)]
+  total_sequences = float(len(mapped_states))
+  initial = [initial_counts[s]/total_sequences for s in range(num_states)]
 
-  paired_states_and_observations = (zip(s_seq, o_seq) for s_seq, o_seq in zip(states, observations))
-  emission_counts = Counter(itertools.chain(*paired_states_and_observations))
-  matrix = [ [emission_counts[(i,j)] for i in range(num_states)] for j in range(num_states)]
-  emissions = []
-  for row in matrix:
-    row_total = float(sum(row))
-    stochastic_row = [count/row_total for count in row]
-    emissions.append(stochastic_row)
+  # Get emissions probability matrix
+  paired_states_and_observations = (zip(s_seq, o_seq) for s_seq, o_seq in zip(mapped_states, mapped_observations))
+  emission_counts = collections.Counter(itertools.chain(*paired_states_and_observations))
+  matrix = [ [emission_counts[(i,j)] for j in range(num_observations)] for i in range(num_states)]
+  emissions = stochasticize(matrix)
+
+  # Get state transition probability matrix
+  state_transitions = itertools.chain(*[zip(sequence, sequence[1:]) for sequence in mapped_states])
+  transition_counts = collections.Counter(state_transitions)
+  matrix = [ [transition_counts[(i,j)] for j in range(num_states)] for i in range(num_states)]
+  transitions = stochasticize(matrix)
+
+  hmm = HiddenMarkovModel(initial, transitions, emissions)
+  return Model(hmm=hmm, o_mapper=observation_mapper, s_mapper=state_mapper)
+
 
 class HiddenMarkovModel(object):
 
@@ -87,6 +106,3 @@ class HiddenMarkovModel(object):
     emission_row_sums = self.emissions.sum(1).flatten().tolist()[0]
     reqs.extend([abs(row_sum - 1) < EPSILON for row_sum in emission_row_sums])
     return all(reqs)
-
-if __name__ == "__main__":
-  ParameterEstimation([["a","b","c"]], [["str1", "str2", "str3"]])
