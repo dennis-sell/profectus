c3fe517ce8aae73112b07ae1210a7d777ea464d7
Dennis Sell <sell.dennis@gmail.com>
2013-07-30 22:43:17 -0700

    Changed to two tabs

--- HMM.py
+++ HMM.py
@@ -3,9 +3,45 @@ import itertools
 import math
 import numpy
 
-EPSILON = 0.000001
 
 Model = collections.namedtuple('Model', ['hmm', 'o_mapper', 's_mapper'])
+
+EPSILON = 0.000001
+
+class HiddenMarkovModel(object):
+
+    def __init__(self, initial, transitions, emissions):
+        """ initial - initial[x] = chance that initial state is state x. (1 x N matrix)
+            transitions - transitions[x][y] = chance that state will transition to y, given that the
+              model is currently at state x. (N x N matrix)
+            emissions - emisions[s][o] = chance that observation o will occur given that the model
+              is at state s. (M x N matrix)
+            N = number of states
+            M = number of observations
+        """
+        self.initial = numpy.matrix(initial)
+        self.transitions = numpy.matrix(transitions)
+        self.emissions = numpy.matrix(emissions)
+        self.N = self.emissions.shape[0]   # Number of states
+        self.M = self.emissions.shape[1]   # Number of obeservations
+        self.states = range(self.N)
+        self.observations = range(self.M)
+
+    def is_valid(self):
+        M = self.M
+        N = self.N
+        # Creates a list of requirements. Then checks if the list meets all of them.
+        reqs = [self.initial.shape == (1,N),
+                self.transitions.shape == (N,N),
+                self.emissions.shape == (N,M),
+                abs(self.initial.sum() - 1) < EPSILON]
+
+        transition_row_sums = self.transitions.sum(1).flatten().tolist()[0]
+        reqs.extend([abs(row_sum - 1) < EPSILON for row_sum in transition_row_sums])
+        emission_row_sums = self.emissions.sum(1).flatten().tolist()[0]
+        reqs.extend([abs(row_sum - 1) < EPSILON for row_sum in emission_row_sums])
+        return all(reqs)
+
 
 class Mapping(dict):
     def __len__(self):
@@ -17,89 +53,56 @@ class Mapping(dict):
 
 
 def get_mapping(list_of_sequences):
-  all_elements = set(itertools.chain(*list_of_sequences))
-  numbered = list(enumerate(list(all_elements)))
-  mapper = Mapping()
-  for e, v in numbered:
-    mapper[e] = v
+    all_elements = set(itertools.chain(*list_of_sequences))
+    numbered = enumerate(list(all_elements))
+    mapper = Mapping()
+    for e, v in numbered:
+        mapper[e] = v
 
-  mapped_sequences = []
-  for sequence in list_of_sequences:
-    mapped_sequences.append([mapper[elem] for elem in sequence])
-  return mapper, mapped_sequences, len(all_elements)
+    mapped_sequences = []
+    for sequence in list_of_sequences:
+        mapped_sequences.append([mapper[elem] for elem in sequence])
+    return mapper, mapped_sequences, len(all_elements)
+
 
 def stochasticize(row, smoothing_constant):
-  row_total = float(sum(row))
-  if row_total != 0:
-    stochastic_row = [count/row_total for count in row]
-  else:
-    stochastic_row = row
-  return stochastic_row
-  
+    row_total = float(sum(row))
+    if row_total != 0:
+        stochastic_row = [count/row_total for count in row]
+    else:
+        stochastic_row = row
+    return stochastic_row
+ 
 
 def stochasticize_matrix(matrix, smoothing_constant):
-  return [stochasticize(row, smoothing_constant) for row in matrix]
+    return [stochasticize(row, smoothing_constant) for row in matrix]
 
 
 def parameter_estimation(training_data, smoothing_constant=.01):
-  observations = [[o for o,s in row] for row in training_data]
-  states = [[s for o,s in row] for row in training_data]
+    observations = [[o for o,s in row] for row in training_data]
+    states = [[s for o,s in row] for row in training_data]
 
-  state_mapper, mapped_states, num_states = get_mapping(states)
-  observation_mapper, mapped_observations, num_observations = get_mapping(observations)
+    state_mapper, mapped_states, num_states = get_mapping(states)
+    observation_mapper, mapped_observations, num_observations = get_mapping(observations)
 
-  # Get initial state matrix
-  first_states = [sequence[0] for sequence in mapped_states]
-  initial_counts = collections.Counter(first_states)
-  initial = [initial_counts[s] for s in range(num_states)]
-  initial = stochasticize(initial, smoothing_constant)
+    # Get initial state matrix
+    first_states = [sequence[0] for sequence in mapped_states]
+    initial_counts = collections.Counter(first_states)
+    initial = [initial_counts[s] for s in range(num_states)]
+    initial = stochasticize(initial, smoothing_constant)
 
-  # Get emissions probability matrix
-  paired_states_and_observations = (zip(s_seq, o_seq) for s_seq, o_seq in zip(mapped_states, mapped_observations))
-  emission_counts = collections.Counter(itertools.chain(*paired_states_and_observations))
-  matrix = [ [emission_counts[(i,j)] for j in range(num_observations)] for i in range(num_states)]
-  emissions = stochasticize_matrix(matrix, smoothing_constant)
+    # Get emissions probability matrix
+    paired_states_and_observations = (zip(s_seq, o_seq) for s_seq, o_seq in zip(mapped_states, mapped_observations))
+    emission_counts = collections.Counter(itertools.chain(*paired_states_and_observations))
+    matrix = [ [emission_counts[(i,j)] for j in range(num_observations)] for i in range(num_states)]
+    emissions = stochasticize_matrix(matrix, smoothing_constant)
 
-  # Get state transition probability matrix
-  state_transitions = itertools.chain(*[zip(sequence, sequence[1:]) for sequence in mapped_states])
-  transition_counts = collections.Counter(state_transitions)
-  matrix = [ [transition_counts[(i,j)] for j in range(num_states)] for i in range(num_states)]
-  transitions = stochasticize_matrix(matrix, smoothing_constant)
+    # Get state transition probability matrix
+    state_transitions = itertools.chain(*[zip(sequence, sequence[1:]) for sequence in mapped_states])
+    transition_counts = collections.Counter(state_transitions)
+    matrix = [ [transition_counts[(i,j)] for j in range(num_states)] for i in range(num_states)]
+    transitions = stochasticize_matrix(matrix, smoothing_constant)
 
-  hmm = HiddenMarkovModel(initial, transitions, emissions)
-  return Model(hmm=hmm, o_mapper=observation_mapper, s_mapper=state_mapper)
+    hmm = HiddenMarkovModel(initial, transitions, emissions)
+    return Model(hmm=hmm, o_mapper=observation_mapper, s_mapper=state_mapper)
 
-
-class HiddenMarkovModel(object):
-
-  def __init__(self, initial, transitions, emissions):
-    """ initial - initial[x] = chance that initial state is state x. (1 x N matrix)
-        transitions - transitions[x][y] = chance that state will transition to y, given that the
-          model is currently at state x. (N x N matrix)
-        emissions - emisions[s][o] = chance that observation o will occur given that the model
-          is at state s. (M x N matrix)
-        N = number of states
-        M = number of observations
-    """
-    self.initial = numpy.matrix(initial)
-    self.transitions = numpy.matrix(transitions)
-    self.emissions = numpy.matrix(emissions)
-    self.N = self.emissions.shape[0]   # Number of states
-    self.M = self.emissions.shape[1]   # Number of obeservations
-    self.states = range(self.N)
-    self.observations = range(self.M)
-
-  def is_valid(self):
-    M = self.M
-    N = self.N
-    # Creates a list of requirements. Then checks if the list meets all of them.
-    reqs = [self.initial.shape == (1,N),
-            self.transitions.shape == (N,N),
-            self.emissions.shape == (N,M),
-            abs(self.initial.sum() - 1) < EPSILON]
-
-    transition_row_sums = self.transitions.sum(1).flatten().tolist()[0]
-    reqs.extend([abs(row_sum - 1) < EPSILON for row_sum in transition_row_sums])
-    emission_row_sums = self.emissions.sum(1).flatten().tolist()[0]
-    reqs.extend([abs(row_sum - 1) < EPSILON for row_sum in emission_row_sums])
-    return all(reqs)
--- HMM_algorithms.py
+++ HMM_algorithms.py
@@ -4,69 +4,69 @@ import operator
 import HMM
 
 def probability_of_observations(hmm, observations):
-  T = len(observations)
-  partial_probability = []
-  first_observation = observations[0]
-  partial_probability = [hmm.initial[0, state] * hmm.emissions[state, first_observation]
+    T = len(observations)
+    partial_probability = []
+    first_observation = observations[0]
+    partial_probability = [hmm.initial[0, state] * hmm.emissions[state, first_observation]
                                                                   for state in hmm.states]
 
-  for t in range(1,T):
-    last_row = partial_probability
-    o = observations[t]
-    partial_probability = []
-    for s2 in hmm.states:
-      partial_probability.append(sum(last_row[s1] * hmm.transitions[s1, s2] for s1 in hmm.states)
+    for t in range(1,T):
+        last_row = partial_probability
+        o = observations[t]
+        partial_probability = []
+        for s2 in hmm.states:
+            partial_probability.append(sum(last_row[s1] * hmm.transitions[s1, s2] for s1 in hmm.states)
                                                                            * hmm.emissions[s2, o])
-  return sum(partial_probability)
+    return sum(partial_probability)
 
 
 def analysis_of_state_sequences(hmm, observations):
 
-  def all_sequences(elements, length):
-    possible_sequences = [[]]
-    for _ in range(length):
-      possible_sequences = [sequence + [n] for sequence in possible_sequences for n in elements]
-    return possible_sequences
+    def all_sequences(elements, length):
+        possible_sequences = [[]]
+        for _ in range(length):
+            possible_sequences = [sequence + [n] for sequence in possible_sequences for n in elements]
+        return possible_sequences
 
-  T = len(observations)
-  state_sequences = all_sequences(hmm.states, T)
+    T = len(observations)
+    state_sequences = all_sequences(hmm.states, T)
 
-  results = []
-  for sequence in state_sequences:
-    probability = hmm.initial[0, sequence[0]] * hmm.emissions[sequence[0], observations[0]]
-    for t in range(1, T):
-      probability *= hmm.transitions[sequence[t - 1], sequence[t]]
-      probability *= hmm.emissions[sequence[t], observations[t]]
-    results.append((sequence, probability))
-  return results
+    results = []
+    for sequence in state_sequences:
+        probability = hmm.initial[0, sequence[0]] * hmm.emissions[sequence[0], observations[0]]
+        for t in range(1, T):
+            probability *= hmm.transitions[sequence[t - 1], sequence[t]]
+            probability *= hmm.emissions[sequence[t], observations[t]]
+        results.append((sequence, probability))
+    return results
 
 
 def vitterbi(hmm, observations):
-  T = len(observations)
-  # Initialization
-  first_observation = observations[0]
-  delta = [hmm.initial[0,state] * hmm.emissions[state, first_observation] for state in hmm.states]
-  omega = [[0] * hmm.N]
-  # Recursion
-  for t in range(1, T):
-    old_delta, delta = delta, []
-    o = observations[t]
-    omega_row = []
-    for s2 in hmm.states:
-      values = enumerate(old_delta[s1] * hmm.transitions[s1, s2] for s1 in hmm.states)
-      max_index, max_value = max(values, key=operator.itemgetter(1))
-      omega_row.append(max_index)
-      delta.append(max_value * hmm.emissions[s2, o])
-    omega.append(omega_row)
+    T = len(observations)
+    # Initialization
+    first_observation = observations[0]
+    delta = [hmm.initial[0,state] * hmm.emissions[state, first_observation] for state in hmm.states]
+    omega = [[0] * hmm.N]
+    # Recursion
+    for t in range(1, T):
+        old_delta, delta = delta, []
+        o = observations[t]
+        omega_row = []
+        for s2 in hmm.states:
+            values = enumerate(old_delta[s1] * hmm.transitions[s1, s2] for s1 in hmm.states)
+            max_index, max_value = max(values, key=operator.itemgetter(1))
+            omega_row.append(max_index)
+            delta.append(max_value * hmm.emissions[s2, o])
+        omega.append(omega_row)
 
-  states = [delta.index(max(delta))]
-  for t in range(1, T):
-    states.insert(0, omega[t][states[0]])
-  return states
+    states = [delta.index(max(delta))]
+    for t in range(1, T):
+        states.insert(0, omega[t][states[0]])
+    return states
 
 
 
 if __name__ == "__main__":
-  hmm = HMM.HiddenMarkovModel([.5,.5],[[.5,.5],[.5,.5]],[[.2,.8], [.8, .2]])
-  for seq, prob in analysis_of_state_sequences(hmm, [0,1,1]):
-    print seq, prob
+    hmm = HMM.HiddenMarkovModel([.5,.5],[[.5,.5],[.5,.5]],[[.2,.8], [.8, .2]])
+    for seq, prob in analysis_of_state_sequences(hmm, [0,1,1]):
+        print seq, prob
